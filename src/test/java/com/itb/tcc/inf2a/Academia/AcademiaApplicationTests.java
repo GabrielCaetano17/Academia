849bc50cc11e94c893b0b0a322ce11e7b6a09d57
Gabriel Caetano <biellcaetano11@gmail.com>
2023-08-24 08:17:12 -0300

    Crianção do ambiente de desonvolvimento

---
+++ src/test/java/com/itb/tcc/inf2a/Academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2a.Academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
